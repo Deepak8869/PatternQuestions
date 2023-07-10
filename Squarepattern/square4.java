04fc543f4e06a965e12a5c96b5de8392731497bb
Deepak8869 <deepak8869880049@gmail.com>
2023-07-10 07:11:18 +0530

    patternQuestions

---
+++ Squarepattern/square4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class square4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       
+        for(int i = 1;i<n;i++){
+              char ch = 'A';
+            for(int j =1 ;j<n;j++){
+                System.out.print(ch);
+                ch = (char)(ch+1);
+            }
+            System.out.println();
+        }
+    }
+}
